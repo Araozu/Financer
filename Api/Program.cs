4b2c0e1d0a759e55930326c368e4a9188a008336
Fernando Araoz <fernando@araozu.dev>
2025-11-03 19:00:44 -0500

    feat: dependencies

--- Api/Program.cs
+++ Api/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -17,10 +16,7 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
