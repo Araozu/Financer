86bfb63cdbc88017d3d0e380e44ec014427d47be
Fernando Araoz <fernando@araozu.dev>
2026-01-12 08:06:39 -0500

    fix: duplicate DB context entity

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -8,8 +8,7 @@ namespace Financer.Infrastructure.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<User>(options)
 {
     public DbSet<Transaction> Transactions { get; set; } = null!;
-    public DbSet<Transaction> Transactions { get; set; }
-    public DbSet<Currency> Currencies { get; set; }
+    public DbSet<Currency> Currencies { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
